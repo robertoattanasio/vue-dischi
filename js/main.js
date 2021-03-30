b27dc10555a5908280f2ccbeef0df59feeff7a17
Roberto <r.attanasio90@gmail.com>
2021-03-30 16:39:45 +0200

    Layout ed importazione dati funzionanti
    Concludo con il prossimo push.

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        albumList: [],
+    },
+
+    created() {
+        axios
+            .get('https://flynn.boolean.careers/exercises/api/array/music')
+            .then((element) => {
+                this.albumList = element.data.response;
+                console.log(this.albumList);
+            })
+    },
+});
