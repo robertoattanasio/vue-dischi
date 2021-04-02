a54d8f05af4f3d17d949cc24d15964a45c2e65de
Roberto <r.attanasio90@gmail.com>
2021-04-02 11:18:05 +0200

    Bonus "selezione generi" completato

--- js/main.js
+++ js/main.js
@@ -2,13 +2,37 @@ var app = new Vue({
     el: '#app',
     data: {
         albumList: [],
+        albumYears: ['All'],
+        albumGenres: ['All'],
+        albumGenresFiltered: [],
+        selectedGenre: 'All'
     },
 
-    created() {
+    methods: {
+        selectGenre: function(selectedGenre) {
+            this.albumGenresFiltered = [];
+            this.albumList.forEach(element => {
+                if (element.genre == selectedGenre) {
+                    this.albumGenresFiltered.push(element);
+                }
+            });
+        }
+    },
+
+    mounted() {
         axios
             .get('https://flynn.boolean.careers/exercises/api/array/music')
             .then((element) => {
                 this.albumList = element.data.response;
+                this.albumList.forEach(album => {
+                    if (!this.albumYears.includes(album.year)) {
+                        this.albumYears.push(album.year);
+                    }
+
+                    if (!this.albumGenres.includes(album.genre)) {
+                        this.albumGenres.push(album.genre);
+                    }
+                });
             })
     },
 });
